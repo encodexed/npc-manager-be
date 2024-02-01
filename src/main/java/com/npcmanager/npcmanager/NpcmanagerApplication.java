892aed6929347fbe751c87479eaf418995465815
Robbie Gollan <robbiegollan@gmail.com>
2024-02-01 12:26:05 +1100

    set up spring

---
+++ src/main/java/com/npcmanager/npcmanager/NpcmanagerApplication.java
@@ -0,0 +1,13 @@
+package com.npcmanager.npcmanager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NpcmanagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NpcmanagerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/npcmanager/npcmanager/NpcmanagerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.npcmanager.npcmanager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NpcmanagerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
