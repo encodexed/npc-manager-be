ff6a3b3a8f4366b25c0a3660d24271e2577b14bd
Robbie Gollan <robbiegollan@gmail.com>
2024-02-01 13:34:58 +1100

    adds npcs entity, seed data

---
+++ src/main/java/com/npcmanager/npcmanager/NPCs/NPC.java
@@ -0,0 +1,80 @@
+package com.npcmanager.npcmanager.NPCs;
+
+import jakarta.annotation.Nonnull;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Table(name = "NPCs")
+@Getter
+@Setter
+public class NPC {
+
+  @Id
+  @Nonnull
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  private Long id;
+
+  @Column
+  @Nonnull
+  private String name;
+
+  @Column
+  @Nonnull
+  private String race;
+
+  @Column
+  @Nonnull
+  private int age;
+
+  @Column
+  private String character_class;
+
+  @Column
+  private int level;
+
+  @Column
+  private String alignment;
+
+  @Column
+  private String backstory;
+
+  @Column
+  private int experience_points;
+
+  @Column
+  private int hit_points;
+
+  @Column
+  private int armor_class;
+
+  @Column
+  private int strength;
+
+  @Column
+  private int dexterity;
+
+  @Column
+  private int constitution;
+
+  @Column
+  private int intelligence;
+
+  @Column
+  private int wisdom;
+
+  @Column
+  private int charisma;
+
+  @Column
+  private String inventory;
+
+  @Column
+  private String notes;
+}
